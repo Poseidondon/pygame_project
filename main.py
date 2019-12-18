9ecaf876b7b1dbca6eb0c22c9516f44a6b1c29b3
Poseidondon <superzarub@rambler.ru>
2019-12-18 22:26:31 +0500

    Player control

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import os
 import pygame
 import random
 import math
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    image = pygame.image.load(fullname).convert()
-
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = pygame.image.load(fullname).convert_alpha()
-    return image
 
 
 class Player(pygame.sprite.Sprite):
@@ -23,18 +9,21 @@ class Player(pygame.sprite.Sprite):
         self.radius = radius
         self.clock = pygame.time.Clock()
         self.image = pygame.Surface((2 * radius, 2 * radius), pygame.SRCALPHA, 32)
-        self.rect = pygame.Rect(x - radius // 2, y - radius // 2, radius * 2, radius * 2)
+        self.rect = pygame.Rect(x - radius, y - radius, radius * 2, radius * 2)
         self.setDir(0)
 
+        self.float_pos = self.float_x, self.float_y = x, y
         self.vx = self.vy = 0
         self.speed = 0
         self.accelerate = 0
         self.rot_speed = 0
         self.dir = 0
 
+        self.forward_accelerate = 150
+        self.backward_accelerate = -50
         self.max_rot_speed = 180
-        self.max_accelerate = 50
-        self.max_speed = 500
+        self.max_speed = 250
+        self.friction = 8
 
     def setDir(self, angle):
         self.image.fill(pygame.SRCALPHA)
@@ -59,7 +48,10 @@ class Player(pygame.sprite.Sprite):
         if not dir:
             dir = self.dir
 
-        pass
+        dir = (dir + 90) % 360
+        self.vx -= math.cos(math.radians(dir)) * force
+        self.vy += math.sin(math.radians(dir)) * force
+        self.speed = math.hypot(self.vx, self.vy)
 
     def update(self):
         time = self.clock.tick() / 1000
@@ -68,7 +60,29 @@ class Player(pygame.sprite.Sprite):
             self.setDir(self.dir)
         if self.accelerate:
             self.addVector(self.accelerate * time, self.dir)
-        self.rect = self.rect.move(self.vx, self.vy)
+        else:
+            if self.speed < 3:
+                self.speed = self.vx = self.vy = 0
+
+        if self.friction:
+            friction = self.friction * time
+            if friction < self.speed:
+                k = (self.speed - friction) / self.speed
+                self.speed -= friction
+                self.vx *= k
+                self.vy *= k
+            else:
+                self.speed = self.vx = self.vy = 0
+        if self.max_speed and self.speed > self.max_speed:
+            k = self.max_speed / self.speed
+            self.speed = self.max_speed
+            self.vx *= k
+            self.vy *= k
+
+        self.float_x += self.vx * time
+        self.float_y -= self.vy * time
+        self.rect.x = self.float_x
+        self.rect.y = self.float_y
 
 
 pygame.init()
@@ -81,16 +95,16 @@ player = Player(400, 300, 16)
 running = True
 
 while running:
-    screen.fill((255, 255, 255))
+    screen.fill((155, 155, 155))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == 273:
-                player.accelerate = player.max_accelerate
+                player.accelerate = player.forward_accelerate
             if event.key == 274:
-                player.accelerate = -player.max_accelerate
+                player.accelerate = player.backward_accelerate
             if event.key == 275:
                 player.rot_speed = player.max_rot_speed
             if event.key == 276:
